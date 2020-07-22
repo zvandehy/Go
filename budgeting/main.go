e0e87c71b27917e8d6a49ef19b85270d80433941
Zeke <zvandehy@gmail.com>
2020-07-22 12:07:19 -0600

    Create and Delete budget items

--- budgeting/main.go
+++ budgeting/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	_"github.com/lib/pq" //postgres driver
 	"net/http"
+	"strconv"
 )
 
 //database type
@@ -59,6 +60,8 @@ func init() {
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/budgetitems", budgetItemsIndex)
+	http.HandleFunc("/budgetitems/create/process", budgetitemsCreateProcess)
+	http.HandleFunc("/budgetitems/delete/process", budgetitemsDeleteProcess)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -99,4 +102,67 @@ func budgetItemsIndex(w http.ResponseWriter, r *http.Request) {
 
 	tpl.ExecuteTemplate(w, "budgetitems.gohtml", budgetItems)
 
+}
+
+func budgetitemsCreateProcess(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// get form values
+	item := BudgetItem{}
+	item.Category = r.FormValue("category")
+	limit :=r.FormValue("expense_limit")
+	item.Frequency = r.FormValue("frequency")
+
+
+	// validate form values
+	if item.Category == "" || limit == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	// convert form values
+	f64, err := strconv.ParseFloat(limit, 32)
+	if err != nil {
+		http.Error(w, http.StatusText(406)+"Please hit back and enter a limit", http.StatusNotAcceptable)
+		return
+	}
+	item.Limit = float32(f64)
+
+	// insert values
+	_, err = db.Exec("INSERT INTO budget_items (category, expense_limit, frequency) VALUES ($1, $2, $3)", item.Category, item.Limit, item.Frequency)
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+
+	//redirect to home
+	http.Redirect(w, r, "/budgetitems", http.StatusSeeOther)
+}
+
+func budgetitemsDeleteProcess(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	
+	id := r.FormValue("id")
+	if id == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+	
+
+	// delete book
+	_, err := db.Exec("DELETE FROM budget_items WHERE item_id=$1;", id)
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/budgetitems", http.StatusSeeOther)
 }
